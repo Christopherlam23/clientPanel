c68444c0e9b4f3a9a18cca913511288c51bc2274
Christopherlam23 <christopherlam@live.com>
2018-11-05 22:26:59 +0400

    first Commit: React-Redux-Firebase Setup + created Dashboard, Sidebar & Clients Component

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Clients extends Component {
+  render() {
+    const clients = [
+      {
+        id: '5156151',
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        email: 'kevin@gmail.com',
+        phone: '555-555-5555',
+        balance: '30'
+      },
+      {
+        id: '3332131',
+        firstName: 'Bob',
+        lastName: 'Jackson',
+        email: 'bob@gmail.com',
+        phone: '555-555-5555',
+        balance: '90'
+      }
+    ];
+
+    if (clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                <i className="fas fa-users" /> Users
+              </h2>
+            </div>
+            <div className="col-md-6" />
+          </div>
+
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>
+                    Rs
+                    {parseFloat(client.balance).toFixed(2)}
+                  </td>
+                  <td>
+                    <Link
+                      to={`/client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" /> Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>Loading...</h1>;
+    }
+  }
+}
+
+export default Clients;
---
+++ src/store.js
@@ -0,0 +1,63 @@
+import { createStore, combineReducers, compose } from 'redux';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
+import { reduxFirestore, firestoreReducer } from 'redux-firestore';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+//Reducers
+// @todo
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCAc5f8FHdHAoxbsqnWasr1qQtJUAYbRuY',
+  authDomain: 'client-panel-01.firebaseapp.com',
+  databaseURL: 'https://client-panel-01.firebaseio.com',
+  projectId: 'client-panel-01',
+  storageBucket: 'client-panel-01.appspot.com',
+  messagingSenderId: '391464457330'
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+};
+
+// Init firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// Init firestore
+const firestore = firebase.firestore();
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig), // firebase instance as first argument
+  reduxFirestore(firebase)
+)(createStore);
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
+});
+
+// Create initial state
+const initialState = {};
+
+// Create store
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  composeWithDevTools(reactReduxFirebase(firebase))
+);
+
+// const store = createStoreWithFirebase(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     reactReduxFirebase(firebase),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+export default store;
